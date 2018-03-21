d343d592eb8b024e4f1278a3eac4210d861c5c2c
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-03-21 12:47:43 +0100

    moved to subdir

--- export-stl.sh
+++
@@ -1,8 +0,0 @@
-#!/ bin/sh
-
-openscad -D '$type="AA"'  -D '$plusterminal_drilling=true'  -D '$minusterminal_drilling=true'  -o battery_dummy-aa-pm.stl battery_dummy.scad
-openscad -D '$type="AA"'  -D '$plusterminal_drilling=true'  -D '$minusterminal_drilling=false' -o battery_dummy-aa-p.stl battery_dummy.scad
-openscad -D '$type="AA"'  -D '$plusterminal_drilling=false' -D '$minusterminal_drilling=true'  -o battery_dummy-aa-m.stl battery_dummy.scad
-openscad -D '$type="AAA"' -D '$plusterminal_drilling=true'  -D '$minusterminal_drilling=true'  -o battery_dummy-aaa-pm.stl battery_dummy.scad
-openscad -D '$type="AAA"' -D '$plusterminal_drilling=true'  -D '$minusterminal_drilling=false' -o battery_dummy-aaa-p.stl battery_dummy.scad
-openscad -D '$type="AAA"' -D '$plusterminal_drilling=false' -D '$minusterminal_drilling=true'  -o battery_dummy-aaa-m.stl battery_dummy.scad
